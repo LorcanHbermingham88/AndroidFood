efd96b09f7b85eefb3c410f399b4b036a0b320d0
lorcan <healylorcan88@gmail.com>
2017-12-13 12:25:37 +0000

    button listeners added

--- app/src/main/java/com/example/healy/androidfood/MainActivity.java
+++ app/src/main/java/com/example/healy/androidfood/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.healy.androidfood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 }
